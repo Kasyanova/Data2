cef6f43edd676a5414a7b8528d98281da7e7dd64
KKasyanova <eliseeva.kseniya@gmail.com>
2022-10-09 15:24:24 +0500

    Задача №12_Работа с данными

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+/*
+Напишите программу, принимающую на вход строку “I like Java!!!”, которая будет выполнять следующие действия:
+
+
+        1. Проверить, содержит ли строка подстроку “Java” (используйте contains()).
+
+        2. Проверить, начинается ли строка с подстроки “I like” (используйте startsWith()).
+
+        3. Проверить, заканчивается ли строка с подставки “!!!” (используйте endsWith()).
+
+        4. Если 3 предыдущих условия верны, выведите данную строку, преобразованную к верхнему регистру.
+
+        5. Замените все символы ‘a’ на ‘о’ и введите подстроку “Jovo” на экран (используйте substring()).
+        */
+package com.max.idea;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Анализируем строку I like Java!!! :");
+        String text = "I like Java!!!";
+
+        //проверка условий
+        boolean one = text.contains("Java");
+        boolean two = text.startsWith("I like");
+        boolean three = text.endsWith("!!!");
+
+        //вывести в верхнем регистре, если все условия верны
+        if (one==true & two==true & three==true) System.out.println(text.toUpperCase());
+
+        //вывести подстроку Jovo - в задаче указано введите, я подумала, что опечатка
+        System.out.println(text.replace('a','o').substring(7,11));
     }
 }
